8b42e802bbe7e72b0edd9eedd8af780f5b98059e
aurea <aurea.b@escolar.ifrn.edu.br>
2024-08-29 15:28:48 -0300

    primeiro commit

---
+++ alunos/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Aluno(models.Model):
+    CIDADES_CHOICES = [
+        ('RN', 'Pau dos Ferros'),
+        ('RN', 'São Miguel'),
+        ('RN', 'Alexandria'),
+        ('RN', 'Tenente Ananias'),
+        ('CE', 'Pereiro')
+    ]
+
+    CURSO_CHOICES = [
+        ('CS', 'Ciência da Computação'),
+        ('ENG', 'Engenharia'),
+        ('MED', 'Medicina'),
+        ('ENF', 'Enfermagem'),
+    ]
+
+    nome = models.CharField(max_length=100)
+    endereco = models.CharField(max_length=255)
+    cidade = models.CharField(max_length=2, choices=CIDADES_CHOICES)
+    email = models.EmailField()
+    curso = models.CharField(max_length=3, choices=CURSO_CHOICES)
+
+    def __str__(self):
+        return self.nome
